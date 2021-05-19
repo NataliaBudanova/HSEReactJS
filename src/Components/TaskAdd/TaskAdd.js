7a557fca587889157643a5c069f28237097b52fd
Буданова Наталья <natalya.budanova@inside.glowbyte.com>
2021-05-19 18:59:52 +0300

    'hw5'

--- src/Components/TaskAdd/TaskAdd.js
+++ src/Components/TaskAdd/TaskAdd.js
@@ -1,13 +1,10 @@
-import styles from './TaskAdd.module.css';
+import styles from './TaskAdd.module.scss';
 import React from 'react';
+import classnames from "classnames/bind"
+import TaskInput from '../Input/Input'
+import { ThemeContext } from "../App/Theme"
 
-const TaskInput = (props) => {
-  return (
-    <div>
-      <input placeholder={props.placeholder} value={props.value} onChange={props.onChange} name={props.name} className={styles.input} />
-    </div>
-  )
-}
+const cx = classnames.bind(styles)
 
 class TaskAdd extends React.Component {
   state = {
@@ -20,24 +17,29 @@ class TaskAdd extends React.Component {
     this.setState({ [name]: value })
   }
 
-  handleAddClick = (props) => {
+  handleAddClick = () => {
     this.props.addNewTask(this.state.name, this.state.description)
   }
 
-  NewTaskButton = () => {
+  addTaskButton = () => {
     return (
-      <div>
-        <button className={styles.button} onClick={this.handleAddClick}>ADD</button>
-      </div>
+      <div className={cx("button_container")}>
+        <ThemeContext.Consumer>
+          {theme => (
+        <button className={cx("button", `button-theme-${theme}`)} onClick={this.handleAddClick}>
+          ADD TASK
+          </button>)}
+        </ThemeContext.Consumer>
+        </div>
     )
   }
 
   render() {
     return (
-      <div>
-        <TaskInput placeholder='Enter task name' value={this.state.name} onChange={this.handleChange} name="name" />
-        <TaskInput placeholder='Enter task description' value={this.state.description} onChange={this.handleChange} name="description" />
-        <this.NewTaskButton />
+      <div className={cx("container")}>
+        <TaskInput placeholder='Name' value={this.state.name} onChange={this.handleChange} name="name" />
+        <TaskInput placeholder='Description' value={this.state.description} onChange={this.handleChange} name="description" />
+        <this.addTaskButton />
       </div>
     )
   }
